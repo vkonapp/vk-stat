125aab4f6d54a65a9f29bad08fa6bbb251db0c93
vkonapp <77061712+vkonapp@users.noreply.github.com>
2021-01-08 23:05:03 +0500

    Updates

---
+++ precache-manifest.a9b0b351991bcaaf1a0bdcbbcd81435f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a3d0cab822c27f5aa49bee845cc8e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94b52df2eb583a5ab254",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "94b52df2eb583a5ab254",
+    "url": "./static/js/2.3fee1503.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.3fee1503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0395b86a274d519dab5f",
+    "url": "./static/js/main.630ca606.chunk.js"
+  },
+  {
+    "revision": "4e8e92cbf8c0ae2ba3ec",
+    "url": "./static/js/runtime-main.fd6aa4e4.js"
+  }
+]);
